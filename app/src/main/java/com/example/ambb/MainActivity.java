7414aacd598e4f5f7afcb2fabaf8c7c874139d66
AMBB <sashamoskalik@gmail.com>
2019-03-12 22:29:57 +0300

    add first version registration and database

--- app/src/main/java/com/example/ambb/MainActivity.java
+++ app/src/main/java/com/example/ambb/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ambb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,7 +82,8 @@ public class MainActivity extends AppCompatActivity
     int id = item.getItemId();
 
     if (id == R.id.nav_personalAccount) {
-      // Handle the camera action
+      Intent intent = new Intent(this, LoginActivity.class);
+      startActivity(intent);// Handle the camera action
     } else if (id == R.id.nav_order) {
 
     } else if (id == R.id.nav_basket) {
