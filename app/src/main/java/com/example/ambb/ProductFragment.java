95a7a02199f52f05e72e031a40c5bafd63dad9e8
AMBB <sashamoskalik@gmail.com>
2019-04-03 20:07:20 +0300

    modify recyclerView and toolbar

--- app/src/main/java/com/example/ambb/ProductFragment.java
+++ app/src/main/java/com/example/ambb/ProductFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
